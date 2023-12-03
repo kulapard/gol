c767438e648b791487de4e0354befa52d276e506
Taras Drapalyuk <taras@drapalyuk.com>
2023-12-03 17:18:26 +0400

    refactor

---
+++ renderer_test.go
@@ -0,0 +1,12 @@
+package main
+
+import "testing"
+
+func TestStdoutRenderer_Render(t *testing.T) {
+	r := StdoutRenderer{}
+	g := NewGameOfLife(1, EmptyLoader{Rows: 3, Cols: 3}, r)
+	if err := g.Load(); err != nil {
+		t.Error("Error loading board:", err)
+	}
+	r.Render(g)
+}
